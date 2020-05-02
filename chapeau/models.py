71d0707ba8dd5aabed4e33fc23bd312d1160db19
Renu Singh <renu.rhino@gmail.com>
2020-05-02 13:41:36 +0200

    Rajoute la primaire cle de la salle.

--- chapeau/models.py
+++ chapeau/models.py
@@ -2,4 +2,4 @@ from django.conf import settings
 from django.db import models
 
 class Salle(models.Model):
-    id = models.CharField(max_length=30)
+    id = models.CharField("Nom de la salle", max_length=30, primary_key=True)
