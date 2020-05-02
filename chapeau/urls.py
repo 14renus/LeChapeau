e3ceb06896a4f2a26778ef9bf0ad8aabe1fcb173
Dasha Valter <dariav@dariav-macbookpro.roam.corp.google.com>
2020-05-02 14:57:46 +0200

    Add form to type salle name

--- chapeau/urls.py
+++ chapeau/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.create_room, name='create_room'),
+    path('post/<str:salle_id>/', views.add_players, name='add_players'),
 ]
--- chapeau/views.py
+++ chapeau/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SalleForm
+from .models import Salle
 
 # Create your views here.
-def create_room(request):
-    return render(request, 'startup.html', {})
+def create_room(request, already_exists=False):
+    print ("welcome")
+    # If the form was submitted
+    if request.method == "POST":
+        form = SalleForm(request.POST)
+        if form.is_valid():
+            salle_id = form.cleaned_data['id']
+            print ('salle_id', salle_id)
+            # Salle with this name already exists, redirect to the initial page
+            if Salle.objects.filter(id = salle_id).exists():
+                print ('already exists!')
+                return redirect('create_room', already_exists=True)
+            salle = form.save()
+            return redirect('add_players', salle_id = salle.id)
+    # If the form was not submitted yet
+    form = SalleForm()
+    return render(request, 'startup.html', {"form" : form, "salle_already_exists" : already_exists})
+
+def add_players(request, salle_id):
+    return render(request, 'add_players.html', {})
 
 
